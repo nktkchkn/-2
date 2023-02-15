5828ff2528d5790711f660cb30e920b61cbc7227
Nikita <nktkchkn21@yandex.ru>
2023-02-16 01:17:00 +0300

    Решил задачу

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿static void Main()
+{
+Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.Read());
+if (a % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
+Console.ReadKey();
+}
